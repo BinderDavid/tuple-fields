2e4ec07a4b1bed6667786472155d0197e7e49c2f
David Binder <david.binder@uni-tuebingen.de>
2023-03-23 12:54:56 +0100

    Add simple tests

--- test/Main.hs
+++ test/Main.hs
@@ -1,4 +1,22 @@
+{-# LANGUAGE OverloadedRecordDot #-}
 module Main (main) where
 
+import Test.Tasty
+import Test.Tasty.HUnit
+import Data.Tuple
+import Data.Tuple.Fields ()
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented."
+main = defaultMain tests
+
+tests :: TestTree
+tests = testGroup "Unit tests" [test1, test2, test3]
+
+test1 :: TestTree
+test1 = testCase "First element of 2-tuple" $ ((True,False)._1 == True) @? "Fail"
+
+test2 :: TestTree
+test2 = testCase "Second element of 2-tuple" $ ((True,False)._2 == False) @? "Fail"
+
+test3 :: TestTree
+test3 = testCase "Projecting on Solo" $ ((Solo True)._1 == True) @? "Fail"
